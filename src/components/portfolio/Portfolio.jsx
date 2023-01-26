5a930b4db12be4b0eb4bd4eb8d40a7e53843e2f7
emmacodes <binma.emma@gmail.com>
2023-01-25 21:43:13 -0800

    added new project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,9 @@ import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.png'
 import IMG4 from '../../assets/portfolio4.png'
-
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.png'
+import IMG7 from '../../assets/portfolio7.png'
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -12,6 +14,7 @@ const Portfolio = () => {
       <h2> Portfolio</h2>
 
       <div className ='container portfolio__container'>
+
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG4} alt="" />
@@ -46,6 +49,38 @@ const Portfolio = () => {
           </div>
         </article>
 
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>Color Scheme Generator</h3>
+          <div className='portfolio__item-cta'>
+          <a href = "https://github.com/EmmaBin/color-scheme-generator" className = 'btn' target='_blank'>Github</a>
+          <a href = "https://super-lily-3287d2.netlify.app/" className = 'btn btn-primary' target='_blank'>Website</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG6} alt="" />
+          </div>
+          <h3>Meme Generator in React</h3>
+          <div className='portfolio__item-cta'>
+          <a href = "https://github.com/EmmaBin/react-meme-generator" className = 'btn' target='_blank'>Github</a>
+          <a href = "https://imaginative-youtiao-eb6869.netlify.app/" className = 'btn btn-primary' target='_blank'>Website</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG7} alt="" />
+          </div>
+          <h3>Quiz in React</h3>
+          <div className='portfolio__item-cta'>
+          <a href = "https://github.com/EmmaBin/quiz-react" className = 'btn' target='_blank'>Github</a>
+          <a href = "https://lambent-starlight-872cbf.netlify.app/" className = 'btn btn-primary' target='_blank'>Website</a>
+          </div>
+        </article>
        
 
      
