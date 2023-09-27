e69d5097c0d7bff6748f2037ae4f7ac28363269e
emmacodes <binma.emma@gmail.com>
2023-09-27 15:12:12 -0700

    Updates

---
+++ precache-manifest.1cd7601ed9c3f57889135fe61442b995.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c2b68408338c207ef55",
+    "url": "/EmmaBin/portfolio/static/css/main.144c1ea5.chunk.css"
+  },
+  {
+    "revision": "6c2b68408338c207ef55",
+    "url": "/EmmaBin/portfolio/static/js/main.6c2b6840.chunk.js"
+  },
+  {
+    "revision": "6d24a2864cc4e80be694",
+    "url": "/EmmaBin/portfolio/static/js/1.6d24a286.chunk.js"
+  },
+  {
+    "revision": "6a45b34cd6d3d6ed1ae9",
+    "url": "/EmmaBin/portfolio/static/js/runtime~main.6a45b34c.js"
+  },
+  {
+    "revision": "a81c9e7c7377c6d72217bc6907dbfd89",
+    "url": "/EmmaBin/portfolio/static/media/me.a81c9e7c.JPG"
+  },
+  {
+    "revision": "56ad698431b832ab4f741ca548d22311",
+    "url": "/EmmaBin/portfolio/static/media/portfolio1.56ad6984.png"
+  },
+  {
+    "revision": "1ca1eb09b5a625704d3b352c8a81c145",
+    "url": "/EmmaBin/portfolio/static/media/portfolio2.1ca1eb09.png"
+  },
+  {
+    "revision": "0acf6161c86281453b631e568ddd2f75",
+    "url": "/EmmaBin/portfolio/static/media/portfolio3.0acf6161.png"
+  },
+  {
+    "revision": "a18b0332e627dd3e31679544fa2ff0c7",
+    "url": "/EmmaBin/portfolio/static/media/portfolio4.a18b0332.png"
+  },
+  {
+    "revision": "32b9f3de992831c108ba86c61b2ad310",
+    "url": "/EmmaBin/portfolio/static/media/portfolio5.32b9f3de.png"
+  },
+  {
+    "revision": "7ffbb36d1a26526178b4a668e6b05cee",
+    "url": "/EmmaBin/portfolio/static/media/portfolio6.7ffbb36d.png"
+  },
+  {
+    "revision": "c3dd3ed1b9eaa1fd0f783411a81548e1",
+    "url": "/EmmaBin/portfolio/static/media/portfolio7.c3dd3ed1.png"
+  },
+  {
+    "revision": "a89ef43bc54db646cdc895d055d74230",
+    "url": "/EmmaBin/portfolio/static/media/skyfall.a89ef43b.png"
+  },
+  {
+    "revision": "b09682c606f61ae075bdf263472d6f79",
+    "url": "/EmmaBin/portfolio/static/media/spec.b09682c6.png"
+  },
+  {
+    "revision": "0cf2207db0b0c78e281d42a92522a125",
+    "url": "/EmmaBin/portfolio/static/media/dumsum.0cf2207d.png"
+  },
+  {
+    "revision": "43441a2cfde4f9a04f2423fe7a2fbf4d",
+    "url": "/EmmaBin/portfolio/static/media/me-about.43441a2c.JPG"
+  },
+  {
+    "revision": "97ccbf6125ddc78d902723b94ef776ce",
+    "url": "/EmmaBin/portfolio/index.html"
+  }
+];
